69b5be4a104cbb9a84d0033a00b3f814c5c75179
lumoko27 <lumoko2@gmail.com>
2024-06-25 19:10:26 -0300

    Created exception handling, production profile and connection to railway

---
+++ src/main/java/destino/destino_viagens_api/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package destino.destino_viagens_api.controller.exception;
+
+import java.util.NoSuchElementException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFoundException(NoSuchElementException businessException) {
+        return new ResponseEntity<>("Resource ID not found", HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handleUnexpectedException(Throwable unexpectedException) {
+        var message =  "Unexpected server error, see the logs";
+        logger.error(message, unexpectedException);
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
